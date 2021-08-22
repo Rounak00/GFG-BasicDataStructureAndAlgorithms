ed1a111e091140103e6f1b7b67461df497c4df25
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-08-22 21:25:13 +0530

    commit

---
+++ Array/6leftrotate.cpp
@@ -0,0 +1,17 @@
+// more efficient
+#include<iostream>
+using namespace std;
+
+int main(){
+   const int n=5;
+    int arr[n]={1,2,3,4,5};
+    int val=arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=val;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
