a6b0b72b9f05aab4d878a0ec1b0419c2489bfc99
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-08-22 21:45:17 +0530

    commit

--- Array/8.0secondlargestelement.cpp
+++ Array/8.0secondlargestelement.cpp
@@ -2,6 +2,34 @@
 #include<iostream>
 using namespace std;
 
+int largest(int arr[],int n)
+{
+  int largest=0;
+  for(int i=1;i<n;i++){
+      if(arr[i]>arr[largest]){
+          largest=i;
+      }
+  }
+return largest;
+}
+
+int sl(int arr[],int n)
+{
+    int larg=largest(arr,n);
+    int res=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr[larg]){
+            if(res==-1){
+                res=i;
+            }
+            else if(arr[i]>arr[res]){
+              res=i;
+            }
+        }
+    }
+    return res;
+}
+
 int main(){
    const int n=5;
     int arr[n]={1,2,2,4,5};
