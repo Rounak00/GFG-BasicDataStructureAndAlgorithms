96315cad4e50dc7ad18a82f0b9766711b24764e5
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-08-22 21:38:55 +0530

    commit

---
+++ Array/8.0secondlargestelement.cpp
@@ -0,0 +1,12 @@
+// more efficient
+#include<iostream>
+using namespace std;
+
+int main(){
+   const int n=5;
+    int arr[n]={1,2,2,4,5};
+    
+    int result=sl(arr,n);
+  cout<<result;
+    return 0;
+}
