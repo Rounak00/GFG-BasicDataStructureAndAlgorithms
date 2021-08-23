6da47b09039f0401e9b21ed97a6b83a344326818
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-08-24 02:45:41 +0530

    commit

--- 2. Recursion/1string-palindrome.cpp
+++ 2. Recursion/1string-palindrome.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+
+bool pal(string &s,int st,int ed){
+    if(st>=ed){return true;}
+    return (s[st]==s[ed])&& pal(s,st+1,ed-1);
+}
+
 int main(){
-    
+    string s="mamiu";
+    int start=0;
+    int end=s.length()-1;
+
+    cout<<  (pal(s,start,end)?"true":"false");
+
+  
+    return 0;
 }
