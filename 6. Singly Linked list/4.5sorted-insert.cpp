f538c9e09becd4adf6fbda061d138258ad194e98
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-09-01 09:15:53 +0530

    commit

---
+++ 6. Singly Linked list/4.5sorted-insert.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+Node *ins(Node *head,int val){
+    Node temp=new Node(val);
+    if(head==NULL){
+        head->next=temp;
+        return head;
+    }
+    if(head->data>val){
+        temp->next=head;
+        return temp;
+    }
+    Node *cur=head;
+    while(cur->next!=NULL && cur->next->data<val){
+      cur=cur->next;
+    }
+    temp->next=cur->next;
+    cur->next=temp;
+    return head;
+}
+void printlist(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=new Node(40);
+   head= ins(head,25);
+	printlist(head);
+	return 0;
+} 
