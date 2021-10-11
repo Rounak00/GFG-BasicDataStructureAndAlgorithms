2469676788830b35b082e6613bf7746170b98438
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-10-12 04:34:00 +0530

    commit

--- 4. Searching/2.1binary-search-recursive.cpp
+++ 4. Searching/2.1binary-search-recursive.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 bool linearsearch(int arr[],int st,int ed,int key){
-  if(st>ed){return false;}
+  if(st<ed){ 
   int mid=(st+ed)/2;
   if(arr[mid]==key){return true;}
   else if(key<arr[mid]){return linearsearch(arr,st,mid-1,key);}
   else {return linearsearch(arr,mid+1,ed,key);}
+  }
+  return -1;
 }
 int main(){
     int arr[5]={1,2,3,4,5};
